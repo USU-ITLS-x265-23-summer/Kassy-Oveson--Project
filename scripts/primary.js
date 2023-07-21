d878ea2bb819dabfb621b25acef4a3161427443f
Kassy Oveson <oveson.kassy@gmail.com>
2023-07-21 15:10:46 -0600

    adjusting top directions and other things

--- scripts/primary.js
+++ scripts/primary.js
@@ -3,14 +3,16 @@ const MAX_GRADE_LEVEL = 9;
 const today = new Date();
 const dateString = today.toLocaleDateString();
 
-const url = "https://api.example.com/date";
+const url = "https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=YOUR_API_KEY";
+const interval = setInterval(() => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const newDateString = data.date;
+      document.getElementById("date").innerHTML = newDateString;
+    });
+}, 1000);
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const newDateString = data.date;
-    document.getElementById("date").innerHTML = newDateString;
-  });
 function validateForm() {
   var gradeLevel = document.getElementById("grade-level").value;
   var teacherName = document.getElementById("teacher-name").value;
